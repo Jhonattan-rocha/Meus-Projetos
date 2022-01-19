3c9a3d285f2f8eff967f81368c761014dcd9c0a5
Jhonattan Rocha da Silva <97904111+Jhonattan-rocha@users.noreply.github.com>
2022-01-19 14:57:42 -0300

    Primeiras anotações do dia

--- Udemy/Python/anotacoesaula2.py
+++ Udemy/Python/anotacoesaula2.py
@@ -2,3 +2,5 @@
 # caractere de escaoe, exemplo: "\
 # qualquer dado vazio retornam false caso faça um casting para bool, e retorna true se não for vazio
 print(bool(""), bool("jhonattan"))
+# ** potência
+# // divisão inteira
