4627e52b3689f8c783484f429542c8b9c41fb075
Jhonattan Rocha da Silva <97904111+Jhonattan-rocha@users.noreply.github.com>
2022-01-19 14:34:37 -0300

    Primeiras anotações do dia

---
+++ Udemy/Python/anotacaoAula1.py
@@ -0,0 +1,6 @@
+print('teste', 'teste2', sep='-')
+
+# sep substitui o espaço entre os argumentos
+# python diferencia Maiúsculas de minúsculas
+# numeros com zero a esquerda é string
+# tudo em python é objeto
---
+++ Udemy/Python/anotacoesaula2.py
@@ -0,0 +1,4 @@
+# python é uma linguagens de tipagem dinâmica
+# caractere de escaoe, exemplo: "\
+# qualquer dado vazio retornam false caso faça um casting para bool, e retorna true se não for vazio
+print(bool(""), bool("jhonattan"))
