26492940316ea1d194c7d2f2317a3ec826c77a99
Jhonattan Rocha da Silva <97904111+Jhonattan-rocha@users.noreply.github.com>
2022-01-22 19:04:49 -0300

    Mudando repositório
    pequena mudança

--- Udemy/Python/anotacaoAula1.py
+++
@@ -1,6 +0,0 @@
-print('teste', 'teste2', sep='-')
-
-# sep substitui o espaço entre os argumentos
-# python diferencia Maiúsculas de minúsculas
-# numeros com zero a esquerda é string
-# tudo em python é objeto
